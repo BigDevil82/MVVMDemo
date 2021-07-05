46153b84f1a0d530d0fa270301ce5209f32d626b
LeoLeng <2321063338@qq.com>
2021-07-05 18:45:14 +0800

    添加项目文件。

---
+++ MVVMDemo/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MVVMDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace MVVMDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ MVVMDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.ViewModels
+{
+    class MainWindowViewModel : NotificationObject
+
+    {
+        private string _input;
+        public string Input
+        {
+            get { return _input; }
+            set { _input = value; this.RaisePropertyChanged("Input"); }
+        }
+
+
+
+
+
+        private string _pattern;
+        public string Pattern
+        {
+            get { return _pattern; }
+            set { _pattern = value; this.RaisePropertyChanged("Pattern"); }
+        }
+
+        private string _result;
+        public string Result
+        {
+            get { return _result; }
+            set { _result = value; this.RaisePropertyChanged("Result"); }
+        }
+
+        public DelegateCommand ExecuteAdd;
+
+        public MainWindowViewModel()
+        {
+            Input = "hhhhh1";
+            ExecuteAdd = new DelegateCommand(new Action(Add));
+        }
+
+        public void Add()
+        {
+            Result = Input + Pattern;
+        }
+
+
+    }
+}
